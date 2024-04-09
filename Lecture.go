6e929344d37e5145a9052ad27d3409761c349428
otacon_mint <les_enfants_terribles@gmx.net>
2024-04-10 00:48:36 +0200

    rearanged files

--- Lecture.go
+++ Lecture.go
@@ -7,3 +7,10 @@ type Lecture struct {
 	dayPattern, timePattern, tittlePattern, roomPattern, commentaryPattern, lecturersPattern, modulesPattern *regexp.Regexp
 	modules, lecturers                                                                                       []string
 }
+
+func newLecture(text string, url string) *Lecture {
+	lec := Lecture{textRaw: text, link: url}
+	lec.dayPattern, _ = regexp.Compile(`([A-Z][a-z])\.\s\d+:\d+`)
+	lec.timePattern, _ = regexp.Compile(`\d+:\d+`)
+	return &lec
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	lec := NewLecture("re", "re")
-	println(lec)
-	RunTutServer()
+	lec := newLecture("re", "re")
+	fmt.Println(lec)
+	//RunTutServer()
 }
