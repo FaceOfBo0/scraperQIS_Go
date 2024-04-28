8164cfbdd5a3f3e3faf2973d3c6d87abcee725a5
otacon_mint <les_enfants_terribles@gmx.net>
2024-04-28 14:11:11 +0200

    small changes

--- Lecture.go
+++ Lecture.go
@@ -1,16 +1,34 @@
 package main
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
-type Lecture struct {
-	day, time, title, flags, room, commentary, lectStr, link, textRaw                                        string
-	dayPattern, timePattern, tittlePattern, roomPattern, commentaryPattern, lecturersPattern, modulesPattern *regexp.Regexp
-	modules, lecturers                                                                                       []string
-}
-
-func newLecture(text string, url string) *Lecture {
-	lec := Lecture{textRaw: text, link: url}
+func NewLecture(text string, url string) *Lecture {
+	lec := &Lecture{textRaw: text, link: url}
 	lec.dayPattern, _ = regexp.Compile(`([A-Z][a-z])\.\s\d+:\d+`)
 	lec.timePattern, _ = regexp.Compile(`\d+:\d+`)
-	return &lec
+	lec.tittlePattern, _ = regexp.Compile(`:\sStartseite\s(.*)\s-\sEinzelansicht`)
+	lec.roomPattern, _ = regexp.Compile(`woch.*?-\s([A-Z][A-Z]\s\d*\.?\d+).*\sGruppe`)
+	lec.modulesPattern, _ = regexp.Compile("BM 1|BM 2|BM 3|AM 1|AM 2|AM 3|AM 4|AM 5|VM 1|VM 2|VM 3|GM 1|GM 2|GM 3")
+	lec.lecturersPattern, _ = regexp.Compile(`Zuständigkeit\s(.+?)\s(Studiengänge\sAbschluss|Zuordnung\szu)+`)
+	lec.commentaryPattern, _ = regexp.Compile(`Inhalt\sKommentar(.*?)\s(Leistungsnachweis|Einsortiert in)`)
+	return lec
+}
+
+type Lecture struct {
+	day, time, title, flags, room, commentary, lectStr, link, textRaw string
+	dayPattern, timePattern, tittlePattern, roomPattern,
+	commentaryPattern, lecturersPattern, modulesPattern *regexp.Regexp
+	modules, lecturers []string
+}
+
+func (l *Lecture) GetLecturers() string {
+	if len(l.lecturers) > 1 {
+		l.lectStr = strings.Join(l.lecturers, ", ")
+	} else if len(l.lecturers) == 1 {
+		l.lectStr = l.lecturers[0]
+	}
+	return l.lectStr
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	lec := newLecture("re", "re")
-	fmt.Println(lec)
+	lec := NewLecture("re", "re")
+	fmt.Println(lec.commentaryPattern.String())
 	//RunTutServer()
 }
