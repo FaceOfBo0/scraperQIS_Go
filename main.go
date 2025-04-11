fabfb85383315e8657475b23a978c4d4c51919cc
Tim <koenigt@stud.uni-frankfurt.de>
2025-04-11 10:31:08 +0200

    deleted comments in main, changed .gitignore

--- main.go
+++ main.go
@@ -2,11 +2,4 @@ package main
 
 func main() {
 	RunServer()
-
-	// const path = "source.xlsx"
-	// infos := getMetaInfos(path)
-	// for _, mi := range infos {
-	// 	fmt.Println(mi)
-	// }
-	// fmt.Println(len(infos))
 }
